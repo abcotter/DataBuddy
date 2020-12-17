ee9ce4162742aee89a51f9bab7b8e8f1461435be
Abigail Cotter <abigail.cotter@gmail.com>
2020-12-16 19:54:44 -0500

    take in file and save to user downloads

--- databuddy.py
+++ databuddy.py
@@ -1,7 +1,35 @@
 import pytesseract as tess
+import sys
+import re
+import csv
 from PIL import Image
+import os
+from pathlib import Path
 
-img = Image.open("test.png")
+name_of_file = sys.argv[2]
+
+path_to_download_folder = str(Path.home() / "Downloads/")
+
+completeName = os.path.join(path_to_download_folder, name_of_file + ".csv")
+
+
+def isfloat(value):
+    try:
+        float(value)
+        return True
+    except ValueError:
+        return False
+
+
+img = Image.open(sys.argv[1])
 text = tess.image_to_string(img)
+result = re.split("\n", text)
+resultList = [float(i.replace(",", "")) for i in result[::-1] if isfloat(i)]
 
-print(text)
+f = open(completeName, "w")
+
+with f:
+
+    writer = csv.writer(f)
+    resultList = [[item] for item in resultList]
+    writer.writerows(resultList)
