6a929e4bfac99caa0d5e13210c7dbe2e5d1a08fe
Abigail Cotter <abigail.cotter@gmail.com>
2020-12-30 13:43:47 -0500

    add copy functionality

--- frontend/src/components/Main.vue
+++ frontend/src/components/Main.vue
@@ -6,6 +6,11 @@
 			Upload an image file for of data, and get it it in a csv form below to
 			skip manual data entry!
 		</h3>
+		<h5>
+			Please note: this uses pytesseract to perform the OCR component. The
+			program is not 100% accurate, so ensure you validate your data after
+			converting to a usable form.
+		</h5>
 		<form action="/action_page.php">
 			<input
 				type="file"
@@ -14,9 +19,12 @@
 				@change="onFileChange"
 			/><br /><br />
 		</form>
-		<div v-if="convertedData">
-			<span v-for="data in convertedData" :key="data">{{ data }} <br /></span>
+		<div v-if="convertedData" class="data" id="data">
+			<span v-for="data in convertedData" :key="data" contenteditable>
+				{{ data }} <br />
+			</span>
 		</div>
+		<button v-if="convertedData" class="copy" @click="copyData">COPY</button>
 	</div>
 </template>
 
@@ -36,6 +44,15 @@ export default {
 			var files = e.target.files || e.dataTransfer.files;
 			if (!files.length) return;
 			this.createImage(files[0]);
+		},
+		copyData() {
+			var copyText = document.getElementById("data").innerText;
+			var input_temp = document.createElement("textarea");
+			input_temp.innerHTML = copyText;
+			input_temp.select();
+			input_temp.setSelectionRange(0, 99999); /*For mobile devices*/
+			document.execCommand("copy");
+			alert(input_temp.value);
 		},
 		createImage(file) {
 			var reader = new FileReader();
@@ -63,7 +80,6 @@ export default {
 					},
 				})
 				.then((result) => {
-					console.log(result);
 					this.convertedData = result.data.result;
 				})
 				.catch((error) => {
@@ -76,4 +92,34 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style>
+.data {
+	width: 800px;
+	height: 400px;
+	background-color: rgb(232, 234, 235);
+	margin: auto;
+	padding-top: 20px;
+	overflow: scroll;
+	position: relative;
+}
+
+.data::-webkit-scrollbar {
+	-webkit-appearance: none;
+}
+
+.data::-webkit-scrollbar:vertical {
+	width: 11px;
+}
+
+.data::-webkit-scrollbar:horizontal {
+	height: 0px;
+}
+
+.data::-webkit-scrollbar-thumb {
+	border-radius: 8px;
+	border: 2px solid rgb(232, 234, 235); /* should match background, can't be transparent */
+	background-color: rgba(0, 0, 0, 0.5);
+}
+
+.copy {
+}
 </style>
